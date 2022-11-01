98401b41ec2d4c352ececd6a865ae3e7b57be73d
koushil-mankali <koushilmankali@gmail.com>
2022-11-01 16:55:38 +0530

    Navbar responsiveness started

--- src/components/Navbar1/Navbar.jsx
+++ src/components/Navbar1/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import css from "./Navbar.module.css";
@@ -7,40 +7,50 @@ import SearchBar from "../../utils/SearchBar/SearchBar";
 import Button1 from "../../utils/Buttons/Button1/Button1";
 
 import globeIcon from "/icons/globe.png";
+import hamburgerIcon from "/icons/hamburger.png";
 
 const Navbar = () => {
+  let [menuState, setMenuState] = useState(false);
+
   return (
     <div className={css.navbar}>
-      <Link className={css.logoBox} to="/">
-        <img src="/images/logo-udemy.svg" alt="logo" className={css.logo} />
-      </Link>
-      <div className={css.catDropdown}>Categories</div>
-      <div className={css.searchBox}>
-        <SearchBar />
+      <div
+        className={css.menuBox}
+        onClick={() => setMenuState((prev) => !prev)}
+      >
+        <img src={hamburgerIcon} alt="menu icon" className={css.menuIcon} />
       </div>
-      <a className={css.hovBox} href="#" target="_blank">
-        Udemy Bussiness
-      </a>
-      <a className={css.hovBox} href="#" target="_blank">
-        Teach on Udemy
-      </a>
-      <Link to="/cart" className={css.cartBox}>
-        <img
-          className={css.cartIcon}
-          src="/icons/shopping-cart.png"
-          alt="cart icon"
-        />
-      </Link>
-      <div className={css.btns}>
-        <Button1 txt="Login" link="/login" />
-        <Button1
-          txt="Sign up"
-          bck="#1c1d1f"
-          link="/signup"
-          color="#fff"
-          hovBck="#000"
-        />
-        <Button1 img={globeIcon} />
+      <div className={css.left}>
+        <Link className={css.logoBox} to="/">
+          <img src="/images/logo-udemy.svg" alt="logo" className={css.logo} />
+        </Link>
+      </div>
+      <div className={css.right}>
+        <div className={css.catDropdown}>Categories</div>
+        <div className={css.searchBox}>
+          <SearchBar />
+        </div>
+        <a className={css.hovBox} href="#" target="_blank">
+          Teach on Udemy
+        </a>
+        <Link to="/cart" className={css.cartBox}>
+          <img
+            className={css.cartIcon}
+            src="/icons/shopping-cart.png"
+            alt="cart icon"
+          />
+        </Link>
+        <div className={css.btns}>
+          <Button1 txt="Login" link="/login" />
+          <Button1
+            txt="Sign up"
+            bck="#1c1d1f"
+            link="/signup"
+            color="#fff"
+            hovBck="#000"
+          />
+          <Button1 img={globeIcon} />
+        </div>
       </div>
     </div>
   );

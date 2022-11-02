968359e065913d9305a86fc1c10b9727c7f2c4f4
koushil-mankali <koushilmankali@gmail.com>
2022-11-02 11:38:51 +0530

    modified

--- src/components/Cards/CourseCard/CourseCard.jsx
+++ src/components/Cards/CourseCard/CourseCard.jsx
@@ -5,24 +5,24 @@ import css from "./CourseCard.module.css";
 import cardImg from "/images/card.jpg";
 
 const CourseCard = (props) => {
-  //   const {img, ttl, authDet, prc, oldprc, rats, stars, noOfRats} = props;
+  //   const {img, ttl="", authDet="", prc=0, oldprc=0, rats=0, stars={}, noOfRats=0} = props;
 
   let img = cardImg;
   let ttl =
     "Learn Python: The Complete Python Programming CourseLearn A-Z everything about Python, from the basics, to advanced topics like Python GUI, Python Data Analysis, and more!Rating: 4.3 out of 53675 reviews14.5 total hours146 lecturesAll LevelsCurrent price: ₹649Original price: ₹3,399";
   let authDet = "Koushil Mankali, Nani";
-  let prc = "649";
-  let oldprc = "1300.20";
-  let rats = "4.3";
+  let prc = 649;
+  let oldprc = 1300.2;
+  let rats = 4.3;
   let stars = { a: 5, b: 1, c: 1, d: 1, e: 1 };
-  let noOfRats = "9";
+  let noOfRats = 9;
   let totalRating = (
-    (1 * stars.a + 2 * stars.b + 3 * stars.c + 4 * stars.d + 5 * stars.e) /
-      stars.a +
-    stars.b +
-    stars.c +
-    stars.d +
-    stars.e
+    (1 * stars?.a + 2 * stars?.b + 3 * stars?.c + 4 * stars?.d + 5 * stars?.e) /
+      stars?.a +
+    stars?.b +
+    stars?.c +
+    stars?.d +
+    stars?.e
   ).toFixed(2);
 
   return (

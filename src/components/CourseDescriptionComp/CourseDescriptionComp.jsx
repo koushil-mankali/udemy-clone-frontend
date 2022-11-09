7ea0595f70e3aebd9cf05fc1c1827c78b5ea0c20
koushil-mankali <koushilmankali@gmail.com>
2022-11-09 15:02:37 +0530

    CourseDescriptionComp css modification (mask-image) (MOD)

--- src/components/CourseDescriptionComp/CourseDescriptionComp.jsx
+++ src/components/CourseDescriptionComp/CourseDescriptionComp.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import { useState } from "react";
 
 import css from "./CourseDescriptionComp.module.css";
 
+import downArrowImg from "/icons/down-arrow.png";
+
 const CourseDescriptionComp = (props) => {
+  const [toggle, setToggle] = useState(false);
+
+  const toggleHandler = () => {
+    setToggle((prev) => !prev);
+  };
+
   const { ttl, desc } = props;
   return (
     <div className={css.outerDiv}>
       <div className={css.ttl}>{ttl}</div>
-      <div className={css.bdy}>{desc}</div>
+      <div className={css.bdy} style={{ height: toggle ? "auto" : "200px" }}>
+        {desc}
+        {toggle ? "" : <div className={css.coverup}></div>}
+      </div>
+      <button className={css.btn} onClick={toggleHandler}>
+        Show more
+        <img
+          style={{ transform: toggle ? "rotate(180deg)" : "none" }}
+          src={downArrowImg}
+          alt="down arrow"
+          className={css.icon}
+        />
+      </button>
     </div>
   );
 };

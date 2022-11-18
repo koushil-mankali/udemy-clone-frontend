de86a9e95855860b8d208abdb78f7150752b4d1e
koushil-mankali <koushilmankali@gmail.com>
2022-11-18 09:43:44 +0530

    VerticalCategoryMenuBar modification started (MOD)

--- src/components/LayoutComponents/VerticalCategoryMenuBar/VerticalCategoryMenuBar.jsx
+++ src/components/LayoutComponents/VerticalCategoryMenuBar/VerticalCategoryMenuBar.jsx
@@ -7,41 +7,41 @@ import rightArrowIcon from "/icons/down-arrow.svg";
 import css from "./VerticalCategoryMenuBar.module.css";
 
 const VerticalCategoryMenuBar = () => {
-  useEffect(() => {
-    document.getElementById("cats").addEventListener("mouseover", (e) => {
-      let subId = "subCat-" + e.target.id?.split("-")[1];
-      let elem = document.getElementById(subId);
-      if (elem) {
-        elem.style = "display:flex";
-        elem.addEventListener("mouseleave", () => {
-          elem.style = "display:none";
-        });
-        document
-          .getElementById("vouterDiv")
-          ?.addEventListener("mouseleave", () => {
-            elem.style = "display:none";
-          });
-      }
-    });
+  // useEffect(() => {
+  //   document.getElementById("cats").addEventListener("mouseover", (e) => {
+  //     let subId = "subCat-" + e.target.id?.split("-")[1];
+  //     let elem = document.getElementById(subId);
+  //     if (elem) {
+  //       elem.style = "display:flex";
+  //       elem.addEventListener("mouseleave", () => {
+  //         elem.style = "display:none";
+  //       });
+  //       document
+  //         .getElementById("vouterDiv")
+  //         ?.addEventListener("mouseleave", () => {
+  //           elem.style = "display:none";
+  //         });
+  //     }
+  //   });
 
-    return () => {
-      document.getElementById("cats").removeEventListener("mouseover", (e) => {
-        let subId = "subCat-" + e.target.id?.split("-")[1];
-        let elem = document.getElementById(subId);
-        if (elem) {
-          elem.style = "display:flex";
-          elem.removeEventListener("mouseleave", () => {
-            elem.style = "display:none";
-          });
-          document
-            .getElementById("vouterDiv")
-            ?.removeEventListener("mouseleave", () => {
-              elem.style = "display:none";
-            });
-        }
-      });
-    };
-  }, []);
+  //   return () => {
+  //     document.getElementById("cats").removeEventListener("mouseover", (e) => {
+  //       let subId = "subCat-" + e.target.id?.split("-")[1];
+  //       let elem = document.getElementById(subId);
+  //       if (elem) {
+  //         elem.style = "display:flex";
+  //         elem.removeEventListener("mouseleave", () => {
+  //           elem.style = "display:none";
+  //         });
+  //         document
+  //           .getElementById("vouterDiv")
+  //           ?.removeEventListener("mouseleave", () => {
+  //             elem.style = "display:none";
+  //           });
+  //       }
+  //     });
+  //   };
+  // }, []);
 
   return (
     <div className={css.outerDiv} id="vouterDiv">
@@ -53,22 +53,25 @@ const VerticalCategoryMenuBar = () => {
                 <div className={css.category} id={`cat-${cat.id}`}>
                   {cat.ttl}
                 </div>
-                <div className={css.subCat} id={cat.subCatId}>
-                  {cat.sub?.map((item) => {
-                    return (
-                      <div
-                        key={`subcat-${item.id}`}
-                        className={css.subCategory}
-                      >
-                        {item.ttl}
-                      </div>
-                    );
-                  })}
-                </div>
               </div>
             );
           })}
         </div>
+        {categoriesSubCategoriesData?.map((cat) => {
+          return (
+            <div className={css.subCat}>
+              {cat.sub?.map((item) => (
+                <div
+                  key={`subcat-${item.id}`}
+                  id="subcat"
+                  className={css.subCategory}
+                >
+                  {item.ttl}
+                </div>
+              ))}
+            </div>
+          );
+        })}
         <div className={css.iconBox}>
           <span>Scroll</span>
           <img src={rightArrowIcon} alt="right arrow" className={css.icon} />

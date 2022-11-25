b04717331a53ad3a815ce82a9e34b0802bcb2273
koushil-mankali <koushilmankali@gmail.com>
2022-11-25 17:23:39 +0530

    CourseSearchResultsComponent component & CategoryCoursePage page structure as per URL modification done (MOD)

--- src/components/CourseSearchResultsComponent/CourseSearchResultsComponent.jsx
+++ src/components/CourseSearchResultsComponent/CourseSearchResultsComponent.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useSearchParams, useParams, useMatch } from "react-router-dom";
 
 import FiltersComp from "../CourseComponents/FiltersComp/FiltersComp";
 import MedVerticalCourseCard from "../Cards/MedVerticalCourseCard/MedVerticalCourseCard";
@@ -12,6 +13,9 @@ import filterIcon from "/icons/filter.png";
 import css from "./CourseSearchResultsComponent.module.css";
 
 const CourseSearchResultsComponent = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { catId } = useParams();
+  const match = useMatch("/courses/search");
   const options = ["Higest Rated", "Most Popular", "Newest"];
   const [selectedOption, setSelectedOption] = useState("");
   const [showFilters, setShowFilters] = useState(false);
@@ -24,6 +28,23 @@ const CourseSearchResultsComponent = () => {
   return (
     <div className={css.outerDiv}>
       <div className={css.innerDiv}>
+        <div className={css.TtlBox}>
+          {!match ? (
+            <div className={css.ttl}>
+              All <span>{catId}</span> courses
+            </div>
+          ) : (
+            <div className={css.ttl}>
+              <span>
+                {new Intl.NumberFormat("en-IN", {
+                  maximumSignificantDigits: 3,
+                }).format("10000")}
+              </span>
+              <span>results for</span>
+              <span>{searchParams.get("q") || null}</span>
+            </div>
+          )}
+        </div>
         <div className={css.topBar}>
           <div className={css.leftTopBar}>
             <Button1
@@ -44,12 +65,14 @@ const CourseSearchResultsComponent = () => {
               onChange={selectFunctionHandler}
               extraCss={{ maxHeight: "55px" }}
             />
-            <div className={css.clAllTxt}>Clear filters</div>
+            <div onClick={() => setSearchParams({})} className={css.clAllTxt}>
+              Clear filters
+            </div>
           </div>
           <div className={css.rightTopBar}>
             {new Intl.NumberFormat("en-IN", {
               maximumSignificantDigits: 3,
-            }).format("10000")}{" "}
+            }).format("10000")}
             results
           </div>
         </div>
--- src/pages/CategoryCoursePage/CategoryCoursePage.jsx
+++ src/pages/CategoryCoursePage/CategoryCoursePage.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useMatch } from "react-router-dom";
 
 import Layout1 from "../Layout1/Layout1";
 import BreadcrumbVerticalCategoryMenuBar from "../../components/LayoutComponents/BreadcrumbVerticalCategoryMenuBar/BreadcrumbVerticalCategoryMenuBar";
@@ -16,33 +16,39 @@ import {
 import css from "./CategoryCoursePage.module.css";
 
 const CategoryCoursePage = () => {
+  const match = useMatch("/courses/search");
   const { catId } = useParams();
+
   return (
     <Layout1>
       <div className={css.outerDiv}>
         <BreadcrumbVerticalCategoryMenuBar />
       </div>
       <div className={css.bdy}>
-        <h2 className={css.pageTtl}>{catId || "Category Page"} Courses</h2>
-        <div className={css.box}>
-          <TabbedCourseCarouselComp ttl="Courses to get you started" />
-        </div>
-        <div className={css.box}>
-          <h2 className={css.ttl}>Featured Courses</h2>
-          <CarouselLayout autoplay={true}>
-            {BigVerticalCourseCardData?.map((item) => {
-              return <BigVerticalCourseCard data={item} key={item.id} />;
-            })}
-          </CarouselLayout>
-        </div>
-        <div className={css.box}>
-          <h2 className={css.ttl}>Popular Instructors</h2>
-          <CarouselLayout slidesToShow={4} slidesToScroll={3}>
-            {popularInstructorsData?.map((item) => {
-              return <InstructorCard data={item} key={item.id} />;
-            })}
-          </CarouselLayout>
-        </div>
+        {!match ? (
+          <>
+            <h2 className={css.pageTtl}>{catId || "Category Page"} Courses</h2>
+            <div className={css.box}>
+              <TabbedCourseCarouselComp ttl="Courses to get you started" />
+            </div>
+            <div className={css.box}>
+              <h2 className={css.ttl}>Featured Courses</h2>
+              <CarouselLayout autoplay={true}>
+                {BigVerticalCourseCardData?.map((item) => {
+                  return <BigVerticalCourseCard data={item} key={item.id} />;
+                })}
+              </CarouselLayout>
+            </div>
+            <div className={css.box}>
+              <h2 className={css.ttl}>Popular Instructors</h2>
+              <CarouselLayout slidesToShow={4} slidesToScroll={3}>
+                {popularInstructorsData?.map((item) => {
+                  return <InstructorCard data={item} key={item.id} />;
+                })}
+              </CarouselLayout>
+            </div>
+          </>
+        ) : null}
         <div className={css.box}>
           <CourseSearchResultsComponent />
         </div>

23b3d3c30bed438083a3eddc4df521c5dba61d8d
koushil-mankali <koushilmankali@gmail.com>
2022-11-29 15:34:08 +0530

    PhotoComponent component (WIP)

--- src/components/UserComponents/Profile/PhotoComponent/PhotoComponent.jsx
+++ src/components/UserComponents/Profile/PhotoComponent/PhotoComponent.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 
+import Button1 from "../../../../utils/Buttons/Button1/Button1";
 import UploadUtil from "../../../../utils/FormUtils/UploadUtil/UploadUtil";
+
+import css from "./PhotoComponent.module.css";
 
 const PhotoComponent = () => {
   return (
-    <div style={{ margin: "2rem" }}>
+    <div className={css.outerDiv}>
+      <div className={css.imgBox}></div>
       <UploadUtil txt="Upload Image" />
+      <Button1
+        txt="Save"
+        bck="var(--light-gray2)"
+        color="var(--white)"
+        hovBck="var(--gray)"
+        extraCss={{
+          padding: "0.5rem",
+          fontSize: "1rem",
+          margin: "2rem 0rem",
+        }}
+      />
     </div>
   );
 };

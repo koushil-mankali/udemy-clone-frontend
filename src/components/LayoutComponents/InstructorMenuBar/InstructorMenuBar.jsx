7d3291f8bd67c103c41c632173c4dda39ef119b3
koushil-mankali <koushilmankali@gmail.com>
2022-11-29 12:20:15 +0530

    InstructorMenuBar component (DONE)

--- src/components/LayoutComponents/InstructorMenuBar/InstructorMenuBar.jsx
+++ src/components/LayoutComponents/InstructorMenuBar/InstructorMenuBar.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import smallLogoIcon from "/images/udemy-icon.svg";
-import logoIcon from "/images/logo-udemy.svg";
 import queryIcon from "/icons/query-question-mark.png";
 import captionIcon from "/icons/caption.png";
-import playTvIcon from "/icons/tv-set.png";
+import playTvIcon from "/icons/tv-monitor.png";
 import analyticsIcon from "/icons/data-analytics.png";
 import settingsIcon from "/icons/settings.png";
 
@@ -14,13 +13,67 @@ import css from "./InstructorMenuBar.module.css";
 const InstructorMenuBar = () => {
   return (
     <div className={css.outerDiv}>
-      <div className={css.iconBox}>
-        <img className={css.icon} src={smallLogoIcon} alt="icon" />
+      <div className={css.menuItem}>
+        <div className={css.iconBox}>
+          <img className={css.icon} src={smallLogoIcon} alt="icon" />
+        </div>
       </div>
       <div className={css.menuBox}>
-        <div className={css.menuItem}>
-          <img className={css.icon} src={queryIcon} alt="icon" />
-        </div>
+        <NavLink
+          to="/user/profile/courses"
+          className={({ isActive }) =>
+            isActive ? [css.menuItem, css.activeLink].join(" ") : css.menuItem
+          }
+        >
+          <div className={css.iconBox}>
+            <img className={css.icon} src={playTvIcon} alt="icon" />
+          </div>
+          <div className={css.menuTxt}>Courses</div>
+        </NavLink>
+        <NavLink
+          to="/user/profile/communication"
+          className={({ isActive }) =>
+            isActive ? [css.menuItem, css.activeLink].join(" ") : css.menuItem
+          }
+        >
+          <div className={css.iconBox}>
+            <img className={css.icon} src={captionIcon} alt="icon" />
+          </div>
+          <div className={css.menuTxt}>Communication</div>
+        </NavLink>
+        <NavLink
+          to="/user/profile/performance"
+          className={({ isActive }) =>
+            isActive ? [css.menuItem, css.activeLink].join(" ") : css.menuItem
+          }
+        >
+          <div className={css.iconBox}>
+            <img className={css.icon} src={analyticsIcon} alt="icon" />
+          </div>
+          <div className={css.menuTxt}>Performance</div>
+        </NavLink>
+        <NavLink
+          to="/user/profile/settings"
+          className={({ isActive }) =>
+            isActive ? [css.menuItem, css.activeLink].join(" ") : css.menuItem
+          }
+        >
+          <div className={css.iconBox}>
+            <img className={css.icon} src={settingsIcon} alt="icon" />
+          </div>
+          <div className={css.menuTxt}>Settings</div>
+        </NavLink>
+        <NavLink
+          to="/user/profile/resources"
+          className={({ isActive }) =>
+            isActive ? [css.menuItem, css.activeLink].join(" ") : css.menuItem
+          }
+        >
+          <div className={css.iconBox}>
+            <img className={css.icon} src={queryIcon} alt="icon" />
+          </div>
+          <div className={css.menuTxt}>Resources</div>
+        </NavLink>
       </div>
     </div>
   );

73231bf4d3089419e3e9339124c6716a69a8c10d
koushil-mankali <koushilmankali@gmail.com>
2022-12-06 17:59:16 +0530

    PlanModalUtil (WIP)

--- src/components/UserComponents/PlayListComponent/PlayListComponent.jsx
+++ src/components/UserComponents/PlayListComponent/PlayListComponent.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
+
+import PlanModalUtil from "../../../utils/Modals/PlanModalUtil/PlanModalUtil";
 
 import editIcon from "/icons/pencil.png";
 import deleteIcon from "/icons/delete.png";
@@ -17,18 +19,64 @@ const PlayListComponent = (props) => {
   const [editModal, setEditModal] = useState(false);
   const [deleteModal, setDeleteModal] = useState(false);
 
+  const modalHandler = (type) => {
+    switch (type) {
+      case "cancel":
+        setDeleteModal(false);
+        setEditModal(false);
+        break;
+      case "edit":
+        setEditModal(false);
+        break;
+      case "ok":
+        setDeleteModal(false);
+        break;
+      default:
+        setDeleteModal(false);
+        setEditModal(false);
+    }
+  };
+
   return (
     <div className={css.outerDiv}>
+      {editModal ? (
+        <PlanModalUtil
+          title=""
+          content=""
+          btns={["cancel", "edit"]}
+          state={editModal}
+          setModal={modalHandler}
+        />
+      ) : null}
+      {deleteModal ? (
+        <PlanModalUtil
+          title="Please confirm"
+          content="Are you sure you want to delete your list? You will still be able to access your courses."
+          btns={["cancel", "ok"]}
+          state={deleteModal}
+          setModal={modalHandler}
+        />
+      ) : null}
       <div className={css.topbar}>
         <div className={css.ttl}>{title}</div>
-        <img src={editIcon} alt="icon" className={css.icon} />
-        <img src={deleteIcon} alt="icon" className={css.icon} />
+        <img
+          src={editIcon}
+          alt="icon"
+          className={css.icon}
+          onClick={() => setEditModal((prev) => !prev)}
+        />
+        <img
+          src={deleteIcon}
+          alt="icon"
+          className={css.icon}
+          onClick={() => setDeleteModal((prev) => !prev)}
+        />
       </div>
       {desc ? <div className={css.desc}>{desc}</div> : null}
       <div className={css.bdy}>
         <div className={css.cards}>
-          {elements?.map((ele) => {
-            return ele;
+          {elements?.map((ele, id) => {
+            return <Fragment key={`ele-${id}`}>{ele}</Fragment>;
           })}
         </div>
         {seeMore ? (
---
+++ src/utils/Modals/PlanModalUtil/PlanModalUtil.jsx
@@ -0,0 +1,67 @@
+import { useEffect } from "react";
+import { createPortal } from "react-dom";
+
+import Button1 from "../../Buttons/Button1/Button1";
+
+import cancelIcon from "/icons/close.png";
+
+import css from "./PlanModalUtil.module.css";
+
+const PlanModalUtil = (props) => {
+  const {
+    title = "",
+    content = "",
+    btns = [],
+    state = false,
+    setModal = () => {},
+  } = props;
+
+  useEffect(() => {
+    console.log(state);
+    if (state) {
+      window.addEventListener("click", (e) => {
+        if (e.target.id !== "modal") {
+          console.log("clicked");
+          return setModal("");
+        }
+      });
+    }
+    return () => {
+      window.removeEventListener("click", (e) => {
+        if (e.target.id !== "modal") {
+          return setModal("");
+        }
+      });
+    };
+  }, []);
+
+  const modal = (
+    <div className={css.outerDiv} id="modal">
+      <div className={css.innerDiv}>
+        <div className={css.bar}>
+          <div className={css.ttl}>{title}</div>
+          <div className={css.imgBox}>
+            <img src={cancelIcon} alt="close icon" className={css.icon} />
+          </div>
+        </div>
+        <div className={css.content}>{content}</div>
+        <div className={css.btns}>
+          {btns?.map((btn, id) => {
+            return (
+              <Button1
+                txt={btn}
+                className={css.btn}
+                key={`btn-${id}`}
+                onClick={() => setModal(btn)}
+                extraCss={{ textTransform: "capitalize" }}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+  return createPortal(modal, document.getElementById("modal"));
+};
+
+export default PlanModalUtil;

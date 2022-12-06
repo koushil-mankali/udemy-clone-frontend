b393fcedb352d73196d473bb44f4e0ac55f4bb88
koushil-mankali <koushilmankali@gmail.com>
2022-12-06 16:28:03 +0530

    PlayListComponent component (WIP)

---
+++ src/components/UserComponents/PlayListComponent/PlayListComponent.jsx
@@ -0,0 +1,8 @@
+import css from "./PlayListComponent.module.css";
+
+const PlayListComponent = (props) => {
+  const { title, elements, seeMore } = props;
+  return <div>PlayListComponent</div>;
+};
+
+export default PlayListComponent;

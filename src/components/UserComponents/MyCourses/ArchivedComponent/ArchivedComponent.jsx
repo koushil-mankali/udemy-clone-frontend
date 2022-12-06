58bbcad8d8cfa6e6417522d696298bc54c17e680
koushil-mankali <koushilmankali@gmail.com>
2022-12-06 13:03:58 +0530

    Working on ArchivedComponent compo (WIP)

--- src/components/UserComponents/MyCourses/ArchivedComponent/ArchivedComponent.jsx
+++ src/components/UserComponents/MyCourses/ArchivedComponent/ArchivedComponent.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import CourseCardWithOptions from "../../../Cards/CourseCardWithOptions/CourseCardWithOptions";
 
-const ArchivedComponent = () => {
-  return <div>ArchivedComponent</div>;
+import { courseDataWithOptions } from "../../../../fakedata/fakedata";
+
+import css from "./ArchivedComponent.module.css";
+
+const ArchivedComponent = (props) => {
+  const optionsComps = [
+    <div className={css.txtBox}>
+      <span className={css.iconBox}>
+        <img src="" alt="icon" className={css.icon} />
+      </span>
+      <span className={css.txt}>Unarchive</span>
+    </div>,
+  ];
+  return (
+    <div className={css.outerDiv}>
+      <div className={css.bdy}>
+        {courseDataWithOptions.map((item) => {
+          return (
+            <CourseCardWithOptions
+              key={item.id}
+              data={item}
+              options={optionsComps}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default ArchivedComponent;

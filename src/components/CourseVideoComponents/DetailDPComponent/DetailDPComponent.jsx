16117bad76f499023deb6fae59bf3ea615d8c4f8
koushil-mankali <koushilmankali@gmail.com>
2022-12-15 14:51:26 +0530

    DetailDPComponent component (DONE)

--- src/components/CourseVideoComponents/DetailDPComponent/DetailDPComponent.jsx
+++ src/components/CourseVideoComponents/DetailDPComponent/DetailDPComponent.jsx
@@ -1,15 +1,52 @@
+import { useState } from "react";
+
 import Button1 from "../../../utils/Buttons/Button1/Button1";
+
+import downArrowIcon from "/icons/down-arrow.svg";
 
 import css from "./DetailDPComponent.module.css";
 
 const DetailDPComponent = (props) => {
-  const { title = "", desc = "", btnTxt = "" } = props;
+  const { title = "", desc = "", btnTxt = "", btnLink = "" } = props;
+
+  const [toggleIcon, setToggleIcon] = useState(false);
+
   return (
-    <details className={css.details}>
-      <summary className={css.summary}>{title}</summary>
-      <p>{desc}</p>
-      {btnTxt ? <Button1 txt={btnTxt} /> : null}
-    </details>
+    <div className={css.outerDiv}>
+      <details
+        className={css.details}
+        onClick={() => setToggleIcon((prev) => !prev)}
+      >
+        <summary className={css.summary}>
+          <span>{title}</span>
+          <span>
+            <img
+              src={downArrowIcon}
+              alt="down arrow"
+              className={[css.icon, toggleIcon ? css.iconReverse : null].join(
+                " "
+              )}
+            />
+          </span>
+        </summary>
+        <p className={css.desc}>{desc}</p>
+        {btnTxt ? (
+          <Button1
+            link={btnLink}
+            txt={btnTxt}
+            color="var(--white)"
+            bck="var(--purple)"
+            hovBck="var(--purple-dark)"
+            extraCss={{
+              padding: "0.7rem",
+              margin: 0,
+              fontSize: "1.1rem",
+              border: "none",
+            }}
+          />
+        ) : null}
+      </details>
+    </div>
   );
 };
 
